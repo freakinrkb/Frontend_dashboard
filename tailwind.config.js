2345ebe23efdfc28c3e100261227405c47276528
Raunak Bhardwaj <ra7nak@gmail.com>
2024-01-01 16:43:06 +0530

    In homne

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  mode: "jit",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mediumslateblue: "#4750eb",
+        gray: {
+          "100": "#f9fbfd",
+          "200": "#0f2137",
+        },
+        plum: "#bfa1f8",
+        lightgreen: "#81dc9a",
+        yellowgreen: "rgba(131, 205, 41, 0.8)",
+        lightsteelblue: "#90a7bb",
+        sandybrown: "#fac874",
+        lightskyblue: "#91afe3",
+        darkslategray: "rgba(44, 71, 99, 0.8)",
+        skyblue: "#56bbd0",
+        black: "#000",
+      },
+      spacing: {},
+      fontFamily: {
+        "dm-sans": "'DM Sans'",
+        arvo: "Arvo",
+        nunito: "Nunito",
+      },
+      borderRadius: {
+        "8xs": "5px",
+      },
+    },
+    fontSize: {
+      "2xl": "21px",
+      inherit: "inherit",
+    },
   },
-  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
