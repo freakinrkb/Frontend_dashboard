2b2fcdda7a796cac74fbb73bd0cdab458a4a3c2f
Piyush Kumar <piyu8h@gmail.com>
2024-01-01 22:08:08 +0530

    Revert "In homne"
    This reverts commit 2345ebe23efdfc28c3e100261227405c47276528.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        mediumslateblue: "#4750eb",
-        gray: {
-          "100": "#f9fbfd",
-          "200": "#0f2137",
-        },
-        plum: "#bfa1f8",
-        lightgreen: "#81dc9a",
-        yellowgreen: "rgba(131, 205, 41, 0.8)",
-        lightsteelblue: "#90a7bb",
-        sandybrown: "#fac874",
-        lightskyblue: "#91afe3",
-        darkslategray: "rgba(44, 71, 99, 0.8)",
-        skyblue: "#56bbd0",
-        black: "#000",
-      },
-      spacing: {},
-      fontFamily: {
-        "dm-sans": "'DM Sans'",
-        arvo: "Arvo",
-        nunito: "Nunito",
-      },
-      borderRadius: {
-        "8xs": "5px",
-      },
-    },
-    fontSize: {
-      "2xl": "21px",
-      inherit: "inherit",
-    },
+    extend: {},
   },
-  corePlugins: {
-    preflight: false,
-  },
+  plugins: [],
 };
