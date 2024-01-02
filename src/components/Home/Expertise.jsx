67fa59a3bb643338294b1a7d4ad5762f9f9cae38
Raunak Bhardwaj <ra7nak@gmail.com>
2024-01-02 19:56:22 +0530

    Expertise Section

--- src/components/Home/Expertise.jsx
+++ src/components/Home/Expertise.jsx
@@ -1,118 +1,77 @@
 import React from "react";
+import ExpertiseCard from "@components/Home/ExpertiseCard.jsx"
+import Wordpress from "@assets/Wordpress 1.png"
+import Flutter from "@assets/flutter 1.png"
+import Ios from "@assets/Frame.png"
+import Android from "@assets/android 1.png"
+import Angular from "@assets/angular 1.png"
+import Node from "@assets/node-js 1.png"
+import react from "@assets/react.png";
+import Uiux from "@assets/vector.png";
 
 const Expertise = () => {
-  return (
-    <main>
-      <div className="relative w-full h-[533px] text-left text-[20px] text-black font-dm-sans">
+  const cardArray = [
+    {
+      iconPath: Uiux,
+      accent: "#56BBD0",
+      title: "UI/UX Design",
+    },
+    {
+      iconPath: Angular,
+      accent: "#2C4763",
+      title: "Angular",
+    }, {
+      iconPath: react,
+      accent: "#FAC873",
+      title: "React",
+    }, {
+      iconPath: Android,
+      accent: "#91AFE3",
+      title: "Android",
+    }, {
+      iconPath: Ios,
+      accent: "#90A7BB",
+      title: "IOS",
+    }, {
+      iconPath: Node,
+      accent: "#83CD29",
+      title: "Node JS",
+    }, {
+      iconPath: Wordpress,
+      accent: "#bfa1f8",
+      title: "Wordpress",
+    },
+    {
+      iconPath: Flutter,
+      accent: "#81DC9A",
+      title: "Flutter",
+    },
 
-        <div className="absolute top-[0px] left-[434px] w-[413px] h-[85px] text-center text-[24px] text-[#0F2137] font-arvo">
-          <b className="absolute top-[35px] left-[0px] tracking-[-0.5px] leading-[50px]">
-          Technology is an Art,We are Artist.
-          </b>
-        </div>
-        <div className="absolute top-[94px] left-[330px] font-medium opacity-[0.5]">
-          Every Business is unique. We pick the right tools for your business
-        </div>
-        <div className="absolute top-[210px] left-[0px] w-[1281px] h-[323px] text-2xl text-[skyblue] font-nunito">
-          <div className="absolute top-[0px] left-[0px] w-[268.7px] h-[133px]">
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-gray-100 w-[267px] h-[133px]" />
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-[#56BBD0] w-[10.4px] h-[133px]" />
-          <div className="absolute top-[84px] left-[41px] font-medium">
-            UI/UX Design
-          </div>
-          <img
-            className="absolute h-[31.2%] w-[36.92%] top-[19.55%] right-[0%] bottom-[49.25%] left-[63.08%] max-w-full overflow-hidden max-h-full object-cover"
-            alt=""
-            src=""
-          />
-        </div>
-        <div className="absolute top-[0px] left-[337px] w-[267px] h-[133px] text-[#2C4763]">
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-gray-100 w-[267px] h-[133px]" />
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-[#2C4763] w-[10.4px] h-[133px]" />
-          <div className="absolute top-[84px] left-[41px] font-medium">
-            Angular
-          </div>
-          <img
-            className="absolute top-[24px] left-[42px] w-[44.9px] h-[44.9px] overflow-hidden object-cover"
-            alt=""
-            src=""
-          />
-        </div>
-        <div className="absolute top-[0px] left-[1014px] w-[267px] h-[133px] text-[#91AFE3]">
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-gray-100 w-[267px] h-[133px]" />
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-[#91AFE3] w-[10.4px] h-[133px]" />
-          <div className="absolute top-[84px] left-[41px] font-medium">
-            Android
-          </div>
-          <img
-            className="absolute top-[24px] left-[41px] w-[44.9px] h-[44.9px] overflow-hidden object-cover"
-            alt=""
-            src=""
-          />
-        </div>
-        <div className="absolute top-[0px] left-[676px] w-[267px] h-[133px] text-[#FAC873]">
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-gray-100 w-[267px] h-[133px]" />
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-[#FAC873] w-[10.4px] h-[133px]" />
-          <img
-            className="absolute top-[24px] left-[41px] w-[44.9px] h-[44.9px] overflow-hidden object-cover"
-            alt=""
-            src=""
-          />
-          <div className="absolute top-[84px] left-[41px] font-medium">
-            React
-          </div>
-        </div>
-        <div className="absolute top-[190px] left-[0px] w-[267px] h-[133px] text-[#90A7BB]">
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-gray-100 w-[267px] h-[133px]" />
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-[#90A7BB] w-[10.4px] h-[133px]" />
-          <div className="absolute top-[84px] left-[41px] font-medium">
-            IOS
+  ]
+
+  return (
+    <section className="py-8">
+      <div className="container">
+        <div className="flex flex-col items-center pb-8">
+            <div className="text-[#4750EB] text-1xl">Expertise</div>
+            <div className="text-[#0F2137] font-arvo text-2xl">
+              <b className=" tracking-[-0.5px] leading-[50px]">
+                Technology is an Art,We are Artist.
+              </b>
             </div>
-          <img
-            className="absolute h-[33.76%] w-[34.12%] top-[17.29%] right-[5.43%] bottom-[48.95%] left-[60.45%] max-w-full overflow-hidden max-h-full object-cover"
-            alt=""
-            src=""
-          />
+            <div className=" font-medium opacity-[0.5]">
+              Every Business is unique. We pick the right tools for your business
+            </div>
         </div>
-        <div className="absolute top-[190px] left-[338px] w-[267px] h-[133px] text-[#83CD29]">
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-gray-100 w-[267px] h-[133px]" />
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-[#83CD29] w-[10.4px] h-[133px]" />
-          <img
-            className="absolute top-[23px] left-[42px] w-[44.9px] h-[44.9px] overflow-hidden object-cover"
-            alt=""
-            src=""
-          />
-          <div className="absolute top-[84px] left-[41px] font-medium">
-            Node JS
-          </div>
-        </div>
-        <div className="absolute top-[190px] left-[1014px] w-[267px] h-[133px] text-[#81DC9A]">
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-gray-100 w-[267px] h-[133px]" />
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-[#81DC9A] w-[10.4px] h-[133px]" />
-          <div className="absolute top-[84px] left-[41px] font-medium">
-            Flutter
-          </div>
-          <img
-            className="absolute top-[23px] left-[41px] w-[44.9px] h-[44.9px] overflow-hidden object-cover"
-            alt=""
-            src=""
-          />
-        </div>
-        <div className="absolute top-[190px] left-[676px] w-[267px] h-[133px] text-[#BFA1F8]">
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-gray-100 w-[267px] h-[133px]" />
-          <div className="absolute top-[0px] left-[0px] rounded-8xs bg-[#BFA1F8] w-[10.4px] h-[133px]" />
-          <div className="absolute top-[84px] left-[41px] font-medium">
-            Wordpress
-          </div>
-          <img
-            className="absolute top-[23px] left-[41px] w-[44.9px] h-[44.9px] overflow-hidden object-cover"
-            alt=""
-            src=".../assets/wordpress 1.png"
-          />
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {
+            cardArray.map((item, index) => (
+              <ExpertiseCard key={index} accent={item.accent} title={item.title} iconPath={item.iconPath} />
+            ))
+          }
         </div>
       </div>
-    </div>
-    </main>
+    </section>
   );
 };
 
