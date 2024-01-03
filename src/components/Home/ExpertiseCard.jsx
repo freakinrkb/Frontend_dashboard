53d6d17e22741d68e3f3383b94bdda1e6eb6d8fa
Piyush Kumar <piyu8h@gmail.com>
2024-01-03 20:26:49 +0530

    Expertise: color fixed

--- src/components/Home/ExpertiseCard.jsx
+++ src/components/Home/ExpertiseCard.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-const ExpertiseCard = ({accent,title,iconPath}) => {
-    
-    return (
-        <div className={`text-[${accent}] relative flex h-36 bg-[#F9FBFD] p-6 px-10 rounded-md`}>
-            <div className={`absolute top-0 left-0 colored h-36 w-2.5 rounded-md bg-[${accent}]`}></div>
-            <div className="flex flex-col justify-between">
-                <img
-                    className="w-10"
-                    alt=""
-                    src={`${iconPath}`}
-                />
-                <div className={`rounded-8xs bg-[${accent}] `} />
-                <div className="text-lg">
-                    {`${title}`}
-                </div>
-            </div>
-        </div>
-    )
-}
+import React from "react";
+const ExpertiseCard = ({ accent, title, iconPath }) => {
+  console.table(accent, iconPath, title);
+  return (
+    <div
+      style={{ color: accent }}
+      className={`relative flex h-36 rounded-md bg-[#F9FBFD] p-6 px-10`}
+    >
+      <div
+        style={{ backgroundColor: accent }}
+        className={`colored absolute left-0 top-0 h-36 w-2.5 rounded-md`}
+      ></div>
+      <div className="flex flex-col justify-between">
+        <img className="w-10" alt="" src={`${iconPath}`} />
+        <div className={`rounded-8xs bg-[${accent}] `} />
+        <div className="text-lg">{`${title}`}</div>
+      </div>
+    </div>
+  );
+};
 
-export default ExpertiseCard
+export default ExpertiseCard;
