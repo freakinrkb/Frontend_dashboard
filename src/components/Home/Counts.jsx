cf19be6e87520d43872c825c1c18cd12ef3927fa
Raunak Bhardwaj <ra7nak@gmail.com>
2024-01-03 22:00:18 +0530

    Counts completed and Domains Updated

--- src/components/Home/Counts.jsx
+++ src/components/Home/Counts.jsx
@@ -2,59 +2,57 @@ import React from "react";
 
 const Counts = () => {
   return (
-    <section>
-      <div className="bg-[#2576BC] container">
-        <div className="flex flex-col items-center">
+    <section className="py -6 bg-[#2576BC]">
+      <div className="container ">
+        <div className="flex flex-col items-center pb-8 pt-8">
           {/* <div className="font-DM Sans"> */}
-            <div className="text-4xl leading-[64px] text-[#FFFFFF] ">
-            <b className="">
-            Some count that matters
-            </b>
+          <div className="text-4xl leading-[64px] text-[#FFFFFF] ">
+            <b className="">Some count that matters</b>
           </div>
           <div className=" text-sm leading-[32px] text-[#FFFFFF]">
-          Our achievement in the journey depicted in numbers
+            Our achievement in the journey depicted in numbers
           </div>
-        {/* </div> */}
+          {/* </div> */}
         </div>
-      <div className="   flex flex-row  gap-[15px]">
-        <div className=" flex flex-col items-center ">
-          <div className=" flex flex-col items-center ">
-            <b className=" text-3xl leading-[64px] inline-block text-[#FFFFFF]">5</b>
-            <div className="text-lg leading-[32px] inline-block text-[#FFFFFF]">
-            Service Geographics
+        <div className="grid grid-cols-1 sm:flex  sm:grid-cols-2 md:grid-cols-4 gap-8 justify-center pb-10">
+          {/* Column 1 */}
+          <div className="flex flex-col items-center">
+            <b className="text-3xl leading-[64px] text-[#FFFFFF]">5</b>
+            <div className="text-lg leading-[32px] text-[#FFFFFF]">
+              Service Geographics
             </div>
           </div>
-        </div>
-        <div className="box-border w-px h-[97px] opacity-[0.4] border-r-[1px] border-solid border-default-white" />
-        <div className=" flex flex-col items-center justify-start">
-          <div className="overflow-hidden flex flex-col items-center justify-start">
-          <b className=" text-3xl leading-[64px] inline-block text-[#FFFFFF]">5</b>
-            <div className="text-lg leading-[32px] inline-block text-[#FFFFFF]">
-            Service Geographics
+          <div className="hidden sm:flex border-default-white box-border h-[97px] w-px border-r-[1px] border-solid opacity-[0.4]" />
+
+          {/* Column 2 */}
+          <div className="flex flex-col items-center">
+            <b className="text-3xl leading-[64px] text-[#FFFFFF]">30</b>
+            <div className="text-lg leading-[32px] text-[#FFFFFF]">
+            Fortune 500 Clients
             </div>
           </div>
-        </div>
-        <div className="box-border w-px h-[97px] opacity-[0.4] border-r-[1px] border-solid border-default-white" />
-        <div className=" flex flex-col items-center justify-start">
-          <div className="overflow-hidden flex flex-col items-center justify-start">
-          <b className=" text-3xl leading-[64px] inline-block text-[#FFFFFF]">5</b>
-            <div className="text-lg leading-[32px] inline-block text-[#FFFFFF]">
-            Service Geographics
+          <div className="hidden sm:flex border-default-white box-border h-[97px] w-px border-r-[1px] border-solid opacity-[0.4]" />
+
+          {/* Column 3 */}
+          <div className="flex flex-col items-center">
+            <b className="text-3xl leading-[64px] text-[#FFFFFF]">300</b>
+            <div className="text-lg leading-[32px] text-[#FFFFFF]">
+            Dedicated Members
             </div>
           </div>
-        </div>
-        <div className="box-border w-px h-[97px] opacity-[0.4] border-r-[1px] border-solid border-default-white" />
-        <div className=" flex flex-col items-center justify-start">
-          <div className="overflow-hidden flex flex-col items-center justify-start">
-          <b className=" text-3xl leading-[64px] inline-block text-[#FFFFFF]">5</b>
-            <div className="text-lg leading-[32px] inline-block text-[#FFFFFF]">
-            Service Geographics
+          <div className="hidden sm:flex border-default-white box-border h-[97px] w-px border-r-[1px] border-solid opacity-[0.4]" />
+
+          {/* Column 4 */}
+          <div className="flex flex-col items-center">
+            <b className="inline-block text-3xl leading-[64px] text-[#FFFFFF]">
+              2
+            </b>
+            <div className="inline-block text-lg leading-[32px] text-[#FFFFFF]">
+            Years of Journey
             </div>
           </div>
         </div>
       </div>
-
-  </div>
     </section>
   );
 };
--- src/components/Home/Domains.jsx
+++ src/components/Home/Domains.jsx
@@ -1,48 +1,54 @@
 import { useState, useEffect } from "react";
 import DomainCard from "@components/Home/DomainCard";
+import icon from "@assets/icon.png";
+import icon2 from "@assets/icon (1).png";
+import icon3 from "@assets/icon (2).png";
+import icon4 from "@assets/icon (3).png";
+import icon5 from "@assets/icon (4).png";
+import icon6 from "@assets/icon (5).png";
 
 const Domains = () => {
   const [isMounted, setIsMounted] = useState(true);
   const [current, setCurrent] = useState(0);
   const domainData = [
     {
-      src: "/images/icon-detailed-records.svg",
-      title: "Detailed Records",
+      src: "icon",
+      title: "Web Design",
       description:
         "Our ability to always exceed the expectations of our clients and produce the most creative output",
       linkto: "#",
     },
     {
-      src: "/images/icon-detailed-records.svg",
-      title: "Detailed Records",
+      src: "icon2",
+      title: "Web Development",
       description:
         "Our ability to always exceed the expectations of our clients and produce the most creative output",
       linkto: "#",
     },
     {
-      src: "/images/icon-fully-customizable.svg",
-      title: "Fully Customizable",
+      src: "icon3",
+      title: "Digital Marketing",
       description:
         "Our ability to always exceed the expectations of our clients and produce the most creative output",
       linkto: "#",
     },
     {
-      src: "/images/icon-brand-recognition.svg",
-      title: "Brand Recognition",
+      src: "icon4",
+      title: "Android Development",
       description:
         "Our ability to always exceed the expectations of our clients and produce the most creative output",
       linkto: "#",
     },
     {
-      src: "/images/icon-detailed-records.svg",
-      title: "Detailed Records",
+      src: "icon5",
+      title: "UI/UX Designer",
       description:
         "Our ability to always exceed the expectations of our clients and produce the most creative output",
       linkto: "#",
     },
     {
-      src: "/images/icon-fully-customizable.svg",
-      title: "Fully Customizable",
+      src: "icon6",
+      title: "iOS Development",
       description:
         "Our ability to always exceed the expectations of our clients and produce the most creative output",
       linkto: "#",
@@ -80,9 +86,8 @@ const Domains = () => {
             title={domainData[current].title}
             description={domainData[current].description}
             linkto={domainData[current].linkto}
-            additionalClass={`bg-white fade-in-out${
-              isMounted ? " fade-in" : " fade-out"
-            }`}
+            additionalClass={`bg-white fade-in-out${isMounted ? " fade-in" : " fade-out"
+              }`}
           />
         </div>
       </div>
