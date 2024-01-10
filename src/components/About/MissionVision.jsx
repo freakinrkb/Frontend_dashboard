b7455b0d5147f2da20b937493f5e2fac28314998
Raunak Bhardwaj <ra7nak@gmail.com>
2024-01-10 10:56:56 +0530

    shadow

--- src/components/About/MissionVision.jsx
+++ src/components/About/MissionVision.jsx
@@ -5,17 +5,17 @@ const MissionVision = () => {
     <section className="py-10 bg-[#F9FBFD]">
       <div className="container ">
         <div class="flex flex-wrap justify-evenly gap-2  px-8 py-10">
-          <div class="box min-w-60 max-w-80 flex-1 bg-white p-10 text-center flex flex-col items-center justify-center">
+          <div class="shadow-[0px_3px_5px_rgba(0,_0,_0,_0.1)] box min-w-60 max-w-80 flex-1 bg-white p-10 text-center flex flex-col items-center justify-center">
             <img className="pb-4" alt="" src={icon} />
             <b className="pb-4">OUR Vision</b>
             Our Vision AI mantra vision is to be recognized as a leading provider of innovative, reliable, and high-quality IT solutions across the globe. We strive to empower businesses of all sizes and industries with cutting-edge technology that enhances their operations, improves their customer experience, and drives their growt
           </div>
-          <div class="box min-w-60 max-w-80 flex-1 bg-white p-10 text-center flex flex-col items-center justify-center">
+          <div class="shadow-[0px_3px_5px_rgba(0,_0,_0,_0.1)] box min-w-60 max-w-80 flex-1 bg-white p-10 text-center flex flex-col items-center justify-center">
             <img className="pb-4" alt="" src={icon} />
             <b className="pb-4">OUR Vision</b>
             Our Vision AI mantra vision is to be recognized as a leading provider of innovative, reliable, and high-quality IT solutions across the globe. We strive to empower businesses of all sizes and industries with cutting-edge technology that enhances their operations, improves their customer experience, and drives their growt
           </div>
-          <div class="box min-w-60 max-w-80 flex-1 bg-white p-10 text-center flex flex-col items-center justify-center">
+          <div class="shadow-[0px_3px_5px_rgba(0,_0,_0,_0.1)] box min-w-60 max-w-80 flex-1 bg-white p-10 text-center flex flex-col items-center justify-center">
             <img className="pb-4" alt="" src={icon} />
             <b className="pb-4">OUR Vision</b>
             Our Vision AI mantra vision is to be recognized as a leading provider of innovative, reliable, and high-quality IT solutions across the globe. We strive to empower businesses of all sizes and industries with cutting-edge technology that enhances their operations, improves their customer experience, and drives their growt
