961b67d8a74fd66e14c2ff38cb8b359d5c5bda81
er-abhisheksharma <er.abhisheksharma.cb@gmmail.com>
2024-01-15 18:06:12 +0530

    update in ifame

--- src/components/Contact/Map.jsx
+++ src/components/Contact/Map.jsx
@@ -1,6 +1,6 @@
 const Map = () => {
   return (
-    <section className="relative my-8 h-60 bg-[url(/src/assets/map-large.png)] bg-cover bg-no-repeat md:h-[20rem]"></section>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3509.429427918526!2d76.99719328941042!3d28.406296509715794!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa72a6435def3cd7%3A0xbcee9a04f03e4cf9!2sAiMantra!5e0!3m2!1sen!2sin!4v1705322059826!5m2!1sen!2sin" width="100%" height="600" style={{border: "0"}} allowfullscreen="" loading="lazy"  />
   );
 };
 
