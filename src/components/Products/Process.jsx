4b0286f54874b226babf2287d9cb76f7480d5bac
Raunak Bhardwaj <ra7nak@gmail.com>
2024-01-27 22:59:28 +0530

    process and stats page is done

--- src/components/Products/Process.jsx
+++ src/components/Products/Process.jsx
@@ -1,5 +1,54 @@
+import React from 'react'
+import idea from '@assets/searchingidea.svg'
+import idea1 from '@assets/idea1.svg'
+import idea2 from '@assets/idea3.svg'
 const Process = () => {
-  return <div>Process</div>;
-};
+  return (
+    <section className='py-16'>
+      <div className='container'>
+        <div className=" font-arvo pb-16 text-center">
+          <div className="font-dm-sans pb-3 text-l font-medium leading-[40px] text-[#4750EB]">
+            Working Proceess
+          </div>
+          <b className=" text-2xl leading-[10px] tracking-[-0.5px]">
+            We complete every step carefully
+          </b>
+        </div>
+        <div className='grid grid-cols-1 sm:grid-cols-3'>
+          <div className='flex'>
+            <div className='px-4'>
+              <img src={idea} alt="" />
+            </div>
+            <div className='flex-col'>
+                <p className='py-2'>Step 1</p>
+                <b className='text-xl py-6'>Searching Idea</b>
+                <p>Get your blood tests delivered at home collect a sample from the news.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div className='px-4'>
+              <img src={idea1} alt="" />
+            </div>
+            <div className='flex-col'>
+                <p className='py-2'>Step 2</p>
+                <b className='text-xl py-6'>Create Prototype</b>
+                <p>Get your blood tests delivered at home collect a sample from the news.</p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div className='px-4'>
+              <img src={idea2} alt="" />
+            </div>
+            <div className='flex-col '>
+                <p className='py-2'>Step 3</p>
+                <b className='text-xl py-6'>Now Start your journey</b>
+                <p>Get your blood tests delivered at home collect a sample from the news.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-export default Process;
+export default Process
--- src/components/Products/Stats.jsx
+++ src/components/Products/Stats.jsx
@@ -1,5 +1,43 @@
+import React from 'react';
+import img1 from '@assets/1.svg';
+import img2 from '@assets/2.svg';
+import img3 from '@assets/3.svg';
+import img4 from '@assets/4.svg';
+
 const Stats = () => {
-  return <div>Stats</div>;
+    return (
+        <section className='py-16 bg-[#EDEEF1]'>
+            <main className='container'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-32 justify-center pb-10'>
+                    <div className='text-center'>
+                        <div className='flex justify-center'><img src={img1} alt="" /></div>
+                        <div className='text -[#2576BC] py-4 text-5xl font-extrabold'>85.45%</div>
+                        <div className='text-[#2576BC] text-l'>Accuracy rate</div>
+                        <div>in fulfilling orders</div>
+                    </div>
+                    <div className='text-center'>
+                        <div className='flex justify-center'><img src={img2} alt="" /></div>
+                        <div className='text -[#2576BC] py-4 text-5xl font-extrabold'>12.5M</div>
+                        <div className='text-[#2576BC] text-l'>On the Inc. 5000</div>
+                        <div>Business Deals</div>
+                    </div>
+                    <div className='text-center'>
+                        <div className='flex justify-center'><img src={img3} alt="" /></div>
+                        <div className='text -[#2576BC] py-4 text-5xl font-extrabold'>3,200+</div>
+                        <div className='text-[#2576BC] text-l'>E-commerce Businesses</div>
+                        <div>happy customers</div>
+                    </div>
+                    <div className='text-center'>
+                        <div className='flex justify-center'><img src={img4} alt="" /></div>
+                        <div className='text -[#2576BC] py-4 text-5xl font-extrabold'>R45</div>
+                        <div className='text-[#2576BC] text-l'>World class</div>
+                        <div>Service Provider</div>
+                    </div>
+                    
+                </div>
+            </main>
+        </section>
+    );
 };
 
 export default Stats;
